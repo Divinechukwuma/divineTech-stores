a3f605c8f3d760a146e448e54223383e07ad051f
divinechukwuma <divinechukwuma982@gmail.com>
2024-05-21 07:51:18 +0100

    i am awesome

--- controller/authorized/admin/show-admin.php
+++ controller/authorized/admin/show-admin.php
@@ -3,10 +3,13 @@
 $config = require 'config.php';
 $db = new Database($config['database']);
 
+
 // Query to fetch admin data based on provided id, name, and email
-$admins = $db->query("SELECT * FROM admin WHERE id = ? AND name = ? AND email = ?")->get(); // Assuming you want to fetch all matching rows
-
-
+$admins = $db->query("SELECT * FROM admin WHERE id = :id, name = :name, email = :email",[
+         'id' => $id,
+         'name' => $name,
+         'email'=> $email
+]); // Assuming you want to fetch all matching rows
 
 // Assuming you want to display the serial number (sn)
 $sn = 1;
